69be56a16c8a913e3532e9d078476a048de48afa
ufgm000 <ufgm000@gmail.com>
2020-02-18 21:22:34 +0900

    スクレイピング

---
+++ sc_chap_1-2.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://www.ymori.com/books/python2nen/test1.html"
+response = requests.get(url)
+
+response.encoding = response.apparent_encoding
+
+filename = 'chap1-2.txt'
+f = open(filename, mode="w")
+f.write(response.text)
+
+f.close()
